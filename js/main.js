cf03cb56a312444e474be39f5245e82d40ef50cc
S-Abdullahi <salabdullahi33@gmail.com>
2022-04-13 17:49:58 +0100

    collapsible navbar

---
+++ js/main.js
@@ -0,0 +1,19 @@
+document.querySelector('.menu').addEventListener('click', menuBar)
+document.querySelector('.clear').addEventListener('click', clearBar)
+
+
+function menuBar(){
+    document.querySelector('.nav-menu').style.left = 0
+    document.querySelector('.menu').style.opacity = 0
+    document.querySelector('.clear').style.opacity = 1
+    document.querySelector('.clear').style.right = '1px'
+
+}
+
+function clearBar(){
+    document.querySelector('.nav-menu').style.left = '-600px'
+    document.querySelector('.clear').style.opacity = 0
+    document.querySelector('.menu').style.opacity = 1
+    document.querySelector('.clear').style.right = '-20px'
+
+}
